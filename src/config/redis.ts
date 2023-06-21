13386169416ce8aedcfd8c105a917debfc1e1fd0
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2023-06-22 00:08:37 +0100

    redis connection fix

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,11 @@
 import { createClient } from "redis";
 
-const host = process.env.REDIS_HOST || "localhost";
+const host = process.env.REDIS_HOST;
 const port = Number(process.env.REDIS_PORT) || 6379;
+const tls = process.env.REDIS_TLS;
+
 const options = {
-	url: `redis://${host}:${port}`,
+	url: `${tls}://${host}:${port}`,
 };
 
 export const client = createClient(options);
