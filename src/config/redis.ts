0d048c1140c9f6e493f38ab2d959a80829da4beb
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2023-06-21 22:45:10 +0100

    changed redis config data

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
-const host = process.env.REDIS_HOST;
-const port = Number(process.env.REDIS_PORT);
+const host = process.env.REDIS_HOST || "localhost";
+const port = Number(process.env.REDIS_PORT) || 6379;
 const options = {
 	url: `redis://${host}:${port}`,
 };
